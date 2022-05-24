96ef49e80daa6fe4f66ff2157aa385ee74b41eee
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-05-24 19:11:31 +0530

    [ index, main ] : works

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,32 @@
-document.addEventListener("readystatechange", (event) => {
-  if (event.target.readyState === "complete") {
-    initApp();
-  }
+/* This is a function that is listening for the DOM to be ready. */
+document.addEventListener("DOMContentLoaded", () => {
+  initApp();
 });
 
 const initApp = () => {
-  console.log("script JS file");
+  model();
+};
+
+// querySelectorAll function
+function qsAll(selector, parent = document) {
+  return [...parent.querySelectorAll(selector)];
+}
+
+// querySelector function
+function qs(selector, parent = document) {
+  return parent.querySelector(selector);
+}
+
+const model = () => {
+  const modal = qs("#modal");
+  const openModal = qs("#openModal");
+  const closeModal = qs("#closeModal");
+
+  openModal.addEventListener("click", () => {
+    modal.showModal();
+  });
+
+  closeModal.addEventListener("click", () => {
+    modal.close();
+  });
 };
